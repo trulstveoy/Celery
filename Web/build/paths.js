65a133566ebf061d740458656a8aa623aee6840a
trulstveoy <trulstveoy@gmail.com>
2015-07-31 10:53:52 +0200

    build files

---
+++ Web/build/paths.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+var appRoot = 'src/';
+var outputRoot = 'dist/';
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.ts',
+  dtssource: 'dts/**/*.d.ts',
+  html: appRoot + '**/*.html',
+  style: 'styles/**/*.css',
+  output: outputRoot,
+  doc:'./doc',
+  e2eSpecsSrc: 'test/e2e/src/*.ts',
+  e2eSpecsDist: 'test/e2e/dist/'
+};
