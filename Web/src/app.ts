471135ade0b3c8c722b0ddc36e15ea484b453b6f
trulstveoy <trulstveoy@gmail.com>
2015-08-06 17:17:58 +0200

    load profile

--- Web/src/app.ts
+++ Web/src/app.ts
@@ -9,10 +9,10 @@ export class App {
         config.title = 'Celery';
         config.addPipelineStep('authorize', AuthorizeStep);
         config.map([
-            { route: 'login', moduleId: './login', nav: false, title: 'Login' },
-            { route: 'logout', moduleId: './logout', nav: false, title: 'Logout' },
-            { route: ['', 'search'], moduleId: './search', nav: true, title: 'Search' },
-            { route: ['admin'], moduleId: './admin', nav: true, title: 'Admin' }
+        { route: 'login', moduleId: './login', nav: false, title: 'Login' },
+        { route: 'logout', moduleId: './logout', nav: false, title: 'Logout' },
+        { route: ['', 'search'], moduleId: './search', nav: true, title: 'Search', auth: true },
+        { route: ['admin'], moduleId: './admin', nav: true, title: 'Admin', auth: true }
         ]);
 
         this.router = router;
--- Web/src/nav-bar.ts
+++ Web/src/nav-bar.ts
@@ -5,13 +5,23 @@ import {AuthService} from 'paulvanbladel/aurelia-auth';
 export class NavBar {
     auth;
     @bindable router = null;
+    profile;
 
     static inject = [AuthService];
     constructor(auth) {
         this.auth = auth;
+
+        this.auth.getMe().then(data => {
+            this.profile = data;
+        });
     }
 
     get isAuthenticated() {
         return this.auth.isAuthenticated();
+    }
+
+    get name() {
+        var a = this.profile;
+        return 'foo';
     }
 }
